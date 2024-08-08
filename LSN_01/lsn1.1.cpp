cde8398b1b85ff27a214cd9e946b7c887213dd7e
Elia Steve <eliaedoardo.steve@studenti.unimi.it>
2024-08-08 16:16:59 +0200

    Final commit for ex. 1.1

--- LSN_01/lsn1.1.cpp
+++ LSN_01/lsn1.1.cpp
@@ -82,20 +82,28 @@ int main() {
 
   //Chi^2 calculation
 
+  //Output file for chi^2
   std::ofstream outputChi2("lsn1.1_chi2s.dat");
   if (!outputChi2.is_open()) {
     std::cerr << "Failed to open output file lsn1.1_chi2.dat. Terminating." << std::endl;
     return -1;
   }
-  int intervalSubdivisions = 100, chi2Trials = 100, valuesPerTrial = 10000;
 
-  double successes[intervalSubdivisions], chi2 = 0;
-  double expectedSuccesses = valuesPerTrial/intervalSubdivisions;
+  int intervalSubdivisions = 100; //Divisions of [0,1]
+  int chi2Trials = 100; //No. of times we'll compute the chi^2
+  int valuesPerTrial = 10000; //Points per computation of chi^2
+
+  //Stores the no. of observed values in each sub-interval
+  double counts[intervalSubdivisions];
+  double chi2 = 0; //Value of chi^2 for a single trial
+  //Expected no. of counts in every subinterval
+  double expectedCount = valuesPerTrial/intervalSubdivisions;
+  //For storing the bin number for single generated values
   int integer_index = 0;
 
-  std::cout << "Chi-squared:" << std::endl;
+  //std::cout << "Chi-squared:" << std::endl;
   for (int i=0; i < chi2Trials; i++) {
-    fillWithZeros(successes, intervalSubdivisions);
+    fillWithZeros(counts, intervalSubdivisions);
     chi2 = 0;
 
     // Run single trial
@@ -103,14 +111,14 @@ int main() {
       val = gen.Rannyu();
       val *= intervalSubdivisions; // 0 <= val < intervalSubdivisions
       integer_index = int(val); // Drops the decimal part, == i if val was in the i-th interval.
-      successes[integer_index]++;
+      counts[integer_index]++;
     }
 
     // Accumulate values for chi2_i
     for (int j=0; j < intervalSubdivisions; j++) {
-      chi2 += pow(successes[j] - expectedSuccesses, 2)/expectedSuccesses;
+      chi2 += pow(counts[j] - expectedCount, 2)/expectedCount;
     }
-    std::cout << i << " " << chi2 << std::endl;
+    //std::cout << i << " " << chi2 << std::endl;
     outputChi2 << chi2 << std::endl;
   }
 
