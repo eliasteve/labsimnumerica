d0a7ea8326db6085cfe20264da3bcb01bafb47b0
Elia Steve <eliaedoardo.steve@studenti.unimi.it>
2024-08-14 19:00:26 +0200

    Last commit (fingers crossed!) for exercise class 3

--- LSN_03/lsn3.1.cpp
+++ LSN_03/lsn3.1.cpp
@@ -2,10 +2,16 @@
 #include <fstream>
 #include <cmath>
 #include "../random/random.h"
+#include "../lib_NSL/misc.h"
 
+//Computes the spot price by sampling a Geometric Brownian Motion
 double getS(double deltaT, double sqrtDeltaT, Random &rng, double S0, double mu, double sigma);
 
-void computeUpdateMeanAndError(int trialsPerBlock, int progressiveIndex, double blockAccumulator, double &meanAccumulator, double &mean2Accumulator, std::ofstream &out);
+//Function to accumulate block values for call, put using only one
+//comperison (instead of calling the max function twice). Not used because
+//upon testing the two versions run in about the same time, and this is
+//less legible.
+void accumulateBlockValues(double T, double r, double S, double K, double& CBlockAccumulator, double& PBlockAccumulator);
 
 int main() {
   double S0 = 100; //Asset price at time 0
@@ -14,59 +20,72 @@ int main() {
   double r = 0.1; //Risk-free interest rate
   double sigma = 0.25; //Volatility
 
-  double sqrtT = sqrt(T); //Cache the sqrt so I don't have to calculate it every time I call S
+  double sqrtT = sqrt(T); //sqrt of T. Cache it so I don't have to calculate it every time I call getS
 
   Random rng("../random/seed.in", "../random/primes32001.in", 1);
 
-  std::ofstream call_direct("lsn3.1_call_direct.dat");
-  if (!call_direct.is_open()) {
-    std::cerr << "Unable to open output file lsn3.1_call_direct.dat. The program terminates." << std::endl;
-    return -1;
-  }
-
-  std::ofstream put_direct("lsn3.1_put_direct.dat");
-  if (!put_direct.is_open()) {
-    std::cerr << "Unable to open output file lsn3.1_put_direct.dat. The program terminates." << std::endl;
-    return -1;
-  }
-
-  std::ofstream call_discretized("lsn3.1_call_discretized.dat");
-  if (!call_discretized.is_open()) {
-    std::cerr << "Unable to open output file lsn3.1_call_discretized.dat. The program terminates." << std::endl;
-    return -1;
-  }
-
-  std::ofstream put_discretized("lsn3.1_put_discretized.dat");
-  if (!put_discretized.is_open()) {
-    std::cerr << "Unable to open output file lsn3.1_put_discretized.dat. The program terminates." << std::endl;
-    return -1;
-  }
-
-
-  int nBlocks = 100, trialsPerBlock = 1000, nTimeIntervals = 100;
-  double deltaT = T/nTimeIntervals, sqrtDeltaT = sqrt(deltaT); //Cache the sqrt
+  int nBlocks = 100; //number of blocks
+  int trialsPerBlock = 1000; //computations of option price per block
+  int nTimeIntervals = 100; //No. of time intervals for discretized
+                            //computation
+  double deltaT = T/nTimeIntervals; //Duration of each time interval
+  double sqrtDeltaT = sqrt(deltaT); //Cached sqrt of deltaT
+  //Block and global accumulators for call value
   double CBlockAccumulator = 0, CMeanAccumulator = 0, CMean2Accumulator = 0;
+  //Block and global accumulators for put value
   double PBlockAccumulator = 0, PMeanAccumulator = 0, PMean2Accumulator = 0;
-  double S = 0;
+  double S = 0; //Spot price
+
+  //Direct calculation
+
+  std::ofstream callOut("call_direct.dat");
+  if (!callOut.is_open()) {
+    std::cerr << "Unable to open output file call_direct.dat. The program terminates." << std::endl;
+    return -1;
+  }
+
+  std::ofstream putOut("put_direct.dat");
+  if (!putOut.is_open()) {
+    std::cerr << "Unable to open output file put_direct.dat. The program terminates." << std::endl;
+    return -1;
+  }
 
   for (int i = 0; i < nBlocks; i++) {
     CBlockAccumulator = 0;
     PBlockAccumulator = 0;
     for (int j = 0; j < trialsPerBlock; j++) {
       S = getS(T, sqrtT, rng, S0, r, sigma);
+      //accumulateBlockValues(T, r, S, K, CBlockAccumulator, PBlockAccumulator);
       CBlockAccumulator += exp(-r*T)*std::max(0., S-K);
       PBlockAccumulator += exp(-r*T)*std::max(0., K-S);
     }
 
-    computeUpdateMeanAndError(trialsPerBlock, i, CBlockAccumulator, CMeanAccumulator, CMean2Accumulator, call_direct);
-    computeUpdateMeanAndError(trialsPerBlock, i, PBlockAccumulator, PMeanAccumulator, PMean2Accumulator, put_direct);
+    CBlockAccumulator /= trialsPerBlock;
+    PBlockAccumulator /= trialsPerBlock;
+
+    computeUpdateMeanAndError(i, CBlockAccumulator, CMeanAccumulator, CMean2Accumulator, callOut);
+    computeUpdateMeanAndError(i, PBlockAccumulator, PMeanAccumulator, PMean2Accumulator, putOut);
 
   }
 
-  call_direct.close();
-  put_direct.close();
+  callOut.close();
+  putOut.close();
 
+  //Discretized calculation
 
+  callOut.open("call_discretized.dat");
+  if (!callOut.is_open()) {
+    std::cerr << "Unable to open output file call_discretized.dat. The program terminates." << std::endl;
+    return -1;
+  }
+
+  putOut.open("put_discretized.dat");
+  if (!putOut.is_open()) {
+    std::cerr << "Unable to open output file put_discretized.dat. The program terminates." << std::endl;
+    return -1;
+  }
+
+  //Reset global accumulators
   CMeanAccumulator = 0;
   CMean2Accumulator = 0;
   PMeanAccumulator = 0;
@@ -80,37 +99,46 @@ int main() {
       for (int k = 0; k < nTimeIntervals; k++) {
         S = getS(deltaT, sqrtDeltaT, rng, S, r, sigma);
       }
+      //accumulateBlockValues(T, r, S, K, CBlockAccumulator, PBlockAccumulator);
       CBlockAccumulator += exp(-r*T)*std::max(0., S-K);
       PBlockAccumulator += exp(-r*T)*std::max(0., K-S);
     }
+    
+    CBlockAccumulator /= trialsPerBlock;
+    PBlockAccumulator /= trialsPerBlock;
 
-    computeUpdateMeanAndError(trialsPerBlock, i, CBlockAccumulator, CMeanAccumulator, CMean2Accumulator, call_discretized);
-    computeUpdateMeanAndError(trialsPerBlock, i, PBlockAccumulator, PMeanAccumulator, PMean2Accumulator, put_discretized);
+    computeUpdateMeanAndError(i, CBlockAccumulator, CMeanAccumulator, CMean2Accumulator, callOut);
+    computeUpdateMeanAndError(i, PBlockAccumulator, PMeanAccumulator, PMean2Accumulator, putOut);
   }
 
-  call_discretized.close();
-  put_discretized.close();
+  callOut.close();
+  putOut.close();
 
   return 0;
 }
 
+//Computes the spot price by sampling a Geometric Brownian Motion
 double getS (
-  double deltaT,
-  double sqrtDeltaT, //deltaT is not going to change across calls of the function, cache its sqrt for efficiency.
-  Random &rng,
-  double S0,
-  double mu,
-  double sigma
+  double deltaT, //Time interval
+  double sqrtDeltaT, //sqrt of time interval: deltaT is not going to change across calls of the function, cache its sqrt for efficiency.
+  Random &rng, //Random number generator
+  double S0, //Value of S at initial time
+  double mu, //Drift
+  double sigma //Diffision constant
 ) {
   return S0 * exp( (mu-pow(sigma, 2)/2.) * deltaT + sigma*rng.Gauss(0, sqrtDeltaT));
 }
 
-void computeUpdateMeanAndError(int trialsPerBlock, int progressiveIndex, double blockAccumulator, double &meanAccumulator, double &mean2Accumulator, std::ofstream &out) {
-  double blockMean, mean;
-
-  blockMean = blockAccumulator/trialsPerBlock;
-  meanAccumulator += blockMean;
-  mean2Accumulator += pow(blockMean, 2);
-  mean = meanAccumulator/(progressiveIndex+1);
-  out << mean << " " << sqrt((mean2Accumulator/(progressiveIndex+1) - pow(mean, 2))/progressiveIndex) << std::endl;
+//Function to accumulate block values for call, put using only one
+//comperison (instead of calling the max function twice). Not used because
+//upon testing the two versions run in about the same time, and this is
+//less legible.
+void accumulateBlockValues(double T, double r, double S, double K, double& CBlockAccumulator, double& PBlockAccumulator) {
+  double difference = S-K;
+  if (difference > 0) {
+    CBlockAccumulator += exp(-r*T)*difference;
+  } else {
+    PBlockAccumulator -= exp(-r*T)*difference;
+  }
+  return;
 }
